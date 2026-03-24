cdbe9801c28e14de59465673b7a321deccae373e
YiGeXiaoZu <850160063@qq.com>
2026-03-24 23:16:06 +0800

    Canonical
    Canonical

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://randomanimalgenerator.com';
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://randomanimalgenerator.online';
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
